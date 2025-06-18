d51e09da38000cdbcdc2bf8912469db776dd8e51
Andrei Stoica <68513477+andreicstoica@users.noreply.github.com>
2025-06-18 18:01:12 -0400

    auth-client fix

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -2,7 +2,8 @@ import { createAuthClient } from "better-auth/react" // make sure to import from
  
 export const authClient =  createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: "http://localhost:3000"
+    //baseURL: "http://localhost:3000"
+    baseURL: 'https://courtly-xi.vercel.app'
 })
 
 export const {
