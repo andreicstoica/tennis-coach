8ef07e784f78f9cdea68844c8414b8d37548fb6b
Andrei Stoica <68513477+andreicstoica@users.noreply.github.com>
2025-06-20 10:35:25 -0400

    oops, forgot to swap auth. fixing this now.

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,9 +1,9 @@
 import { createAuthClient } from "better-auth/react"; // make sure to import from better-auth/react
+import "dotenv/config";
 
 export const authClient = createAuthClient({
   /** The base URL of the server (optional if you're using the same domain) */
-  baseURL: "http://localhost:3000",
-  //baseURL: "https://courtly-xi.vercel.app",
+  baseURL: process.env.NEXT_PUBLIC_AUTH_API_URL ?? "http://localhost:3000",
 });
 
 export const { signIn, signUp, signOut, useSession, getSession } = authClient;
