ac25bb4ae210eb9335f8a9e020079575b7db63e4
Andrei Stoica <68513477+andreicstoica@users.noreply.github.com>
2025-06-20 10:13:33 -0400

    sentry implemented and default page tested

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -2,8 +2,8 @@ import { createAuthClient } from "better-auth/react"; // make sure to import fro
 
 export const authClient = createAuthClient({
   /** The base URL of the server (optional if you're using the same domain) */
-  //baseURL: "http://localhost:3000",
-  baseURL: "https://courtly-xi.vercel.app",
+  baseURL: "http://localhost:3000",
+  //baseURL: "https://courtly-xi.vercel.app",
 });
 
 export const { signIn, signUp, signOut, useSession, getSession } = authClient;
