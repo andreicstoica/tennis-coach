acdcd52228c16ce4b2998b08d1c9ea61685b7ed5
Andrei Stoica <68513477+andreicstoica@users.noreply.github.com>
2025-06-20 17:48:16 -0400

    trying to debug google auth and protecting an extra route to auth sessions

--- src/app/practice-session/page.tsx
+++ src/app/practice-session/page.tsx
@@ -3,9 +3,18 @@
 import { useRef } from "react";
 import { BouncingBallAnimation } from "~/components/bouncing-ball";
 import NewPracticeSessionForm from "~/components/new-practice-session-form";
+import { useSession } from "~/lib/auth-client";
+import { useRouter } from "next/navigation";
 
 export default function NewPracticeSession() {
   const formRef = useRef<HTMLDivElement | null>(null);
+
+  const { data: session } = useSession();
+  const router = useRouter();
+
+  if (!session?.user.id) {
+    router.push("/");
+  }
 
   return (
     <div className="flex min-h-full flex-col items-center justify-center">
